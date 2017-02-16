e2c3ab6f6b4b7846c6382b731c9822bf6845e6da
Hermann Hafsteinsson <hermannhaf@gmail.com>
2017-02-16 12:44:04 +0000

    Bætt við adventure ferðum

--- src/app.js
+++ src/app.js
@@ -3,18 +3,10 @@
 var express = require('express');
 var parser = require('body-parser');
 var router = require('./routes');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
 var jsonfile = require('jsonfile');
 
 var app = express();
 
-// Mongodb connection
-mongoose.connect("mongodb://localhost:27017/booking-app");
-var db = mongoose.connection;
-// mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
 
 
 // Use public
